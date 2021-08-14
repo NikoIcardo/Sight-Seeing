3733ebb14594923290cf4d52dae859568e293424
Glagny <nikoicardo@gmail.com>
2021-08-13 19:46:51 -0600

    Added the Login Logic

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -59,10 +59,34 @@ const Auth = () => {
   const authSubmitHandler =  async event => {
     event.preventDefault();
     
+    setIsLoading(true);
     if(isLoginMode){
+      try {
+        const response = await fetch('http://localhost:5000/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          }, 
+          body: JSON.stringify({ 
+            email: formState.inputs.email.value, 
+            password: formState.inputs.password.value
+          })
+        });
+
+        const responseData = response.json();
+        // the following is a response object property that is true if there is a 200ish status code. 
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        console.log(responseData);
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || 'Something went wrong, please try again.');
+      }
     } else {
       try {
-        setIsLoading(true); 
         const response = await fetch('http://localhost:5000/api/users/signup/', {
           method: 'POST',
           headers: {
