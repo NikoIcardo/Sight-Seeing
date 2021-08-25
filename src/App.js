33a58a9a9e0d914d81ccf4a39c968f23d1009781
Glagny <nikoicardo@gmail.com>
2021-08-25 07:54:38 -0600

    Added Auto-login and Auto-logout
    Auto-Login: If a login is performed and then the page reloads, we auto-log the user back in, which 'keeps' the user logged in.
    
    Auto-Logout: We set a logout timer that logs the user out automatically and clears the api access token after one hour.

--- src/App.js
+++ src/App.js
@@ -15,28 +15,59 @@ import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
-    localStorage.setItem('userData', JSON.stringify({userId: uid, token: token}));
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); //generate a token expiration date one hour after time of login if this timer was not already set. 
+    setTokenExpirationDate(tokenExpirationDate); // set this token as a state so that we can use it as a timeout timer in use effect below
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString()
+      })
+    );
   }, []);
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem('userData'));
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
-    }
-  }, [login]);
 
   const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId();
     localStorage.removeItem('userData');
   }, []);
+
+  // calculate timeout. 
+  useEffect(() => {
+    if(token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer); // in case we logout manually
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  // Allow login state to persist through reloads. 
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(storedData.userId, storedData.token, new Date(storedData.expiration));
+    }
+    console.log(storedData);
+  }, [login]);
+
 
   let routes;
 
@@ -79,7 +110,7 @@ const App = () => {
     <AuthContext.Provider
       value={{
         isLoggedIn: !!token,
-        token: token, 
+        token: token,
         userId: userId,
         login: login,
         logout: logout,
